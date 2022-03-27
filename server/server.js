747b708eaed1fab01b19649f1a55ddf6d6c9f8a8
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2022-03-27 08:45:05 -0400

    wip

--- server/server.js
+++ server/server.js
@@ -1,11 +1,67 @@
 require('dotenv').config()
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 
-app.get('/getRestaurants', (req, res)=>{
-    console.log('get all restaurants')
+app.use(express.json())
+
+//Get all restaurants
+app.get('/api/v1/restaurants', (req, res)=>{
+    console.log('route handler ran')
+    res.status(200).json({
+        'status': 'success',
+        data: {
+            'restaurant': [
+                'cleveland vegan',
+                'melt'
+            ]
+        }
+        
+    })
 })
+
+//get a single restaurant
+app.get('/api/v1/restaurants/:id', (req, res) =>{
+    console.log(req.params)
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: 'boss chicken'
+        }
+    })
+})
+
+//Create a restaurant
+app.post('/api/v1/restaurants', (req, res)=>{
+    console.log(req.body)
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: 'boss chicken'
+        }
+    })
+})
+
+//Update restaurants
+app.put("/api/v1/restaurants/:id", (req, res)=>{
+    console.log(req.params.id)
+    console.log(req.body)
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: 'boss chicken'
+        }
+    })
+})
+
+//Delete restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) =>{
+    res.status(204).json({
+        status: 'restaurant deleted'
+    })
+})
+
 
 const PORT  = process.env.PORT || 3000;
 app.listen(3000, ()=>{
